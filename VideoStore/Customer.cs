334f12b22441175aaf4dd7d63c12c63424672652
Corey Greene <cgreene@qualtrax.com>
2021-08-10 10:59:59 -0400

    initial commit, moved CalculatePrice & CalculateFrequentRenterPoints into Rental.cs

--- VideoStore/Customer.cs
+++ VideoStore/Customer.cs
@@ -5,6 +5,9 @@ namespace VideoStore
 {
     public class Customer
     {
+        private String name;
+        private List<Rental> rentals = new List<Rental>();
+
         public Customer(string name)
         {
             this.name = name;
@@ -28,47 +31,18 @@ namespace VideoStore
 
             foreach (var rental in rentals)
             {
-                double thisAmount = 0;
+                double thisAmount = rental.CalculatePrice();
 
-                // determines the amount for each line
-                switch (rental.GetMovie().GetPriceCode())
-                {
-                    case Movie.REGULAR:
-                        thisAmount += 2;
-                        if (rental.GetDaysRented() > 2)
-                            thisAmount += (rental.GetDaysRented() - 2) * 1.5;
-                        break;
-                    case Movie.NEW_RELEASE:
-                        thisAmount += rental.GetDaysRented() * 3;
-                        break;
-                    case Movie.CHILDRENS:
-                        thisAmount += 1.5;
-                        if (rental.GetDaysRented() > 3)
-                            thisAmount += (rental.GetDaysRented() - 3) * 1.5;
-                        break;
-                }
-
-                frequentRenterPoints++;
-
-                if (rental.GetMovie().GetPriceCode() == Movie.NEW_RELEASE
-                        && rental.GetDaysRented() > 1)
-                    frequentRenterPoints++;
+                frequentRenterPoints += rental.CalculateFrequentRenterPoints();
 
                 result += "\t" + rental.GetMovie().GetTitle() + "\t"
                                     + thisAmount + "\n";
                 totalAmount += thisAmount;
-
             }
 
             result += "You owed " + totalAmount + "\n";
             result += "You earned " + frequentRenterPoints + " frequent renter points\n";
-
-
             return result;
         }
-
-
-        private String name;
-        private List<Rental> rentals = new List<Rental>();
     }
 }
--- VideoStore/Rental.cs
+++ VideoStore/Rental.cs
@@ -2,15 +2,13 @@
 {
     public class Rental
     {
+        private Movie movie;
+        private int daysRented;
+
         public Rental(Movie movie, int daysRented)
         {
             this.movie = movie;
             this.daysRented = daysRented;
-        }
-
-        public int GetDaysRented()
-        {
-            return daysRented;
         }
 
         public Movie GetMovie()
@@ -18,7 +16,34 @@
             return movie;
         }
 
-        private Movie movie;
-        private int daysRented;
+        public int CalculateFrequentRenterPoints()
+        {
+            return (movie.GetPriceCode() == Movie.NEW_RELEASE && daysRented > 1) ? 2 : 1;
+        }
+
+        public double CalculatePrice()
+        {
+            double thisAmount = 0;
+
+            // determines the amount for each line
+            switch (movie.GetPriceCode())
+            {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (daysRented > 2)
+                        thisAmount += (daysRented - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += daysRented * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (daysRented > 3)
+                        thisAmount += (daysRented - 3) * 1.5;
+                    break;
+            }
+            return thisAmount;
+        }
+
     }
 }
